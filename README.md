2d3515f9d6d5aa9dd2616aeaecd652cc6d694b2d
Mir <mir.m.ali01@gmail.com>
2023-04-01 13:12:00 -0400

    added a view engine, homepage, and readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Create an Express Student Grader
+
+Use express-routes and JSX to display all students and functionality to create new students.
+Also each student must have grades and create the functionality to add grades to each student as well.
--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// Set up view engine above all routes, enables rendering from views fldr
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+
+// Get
+app.get("/", (req, res) => {
+  res.send("What's good!");
+});
+
 // Listen
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
